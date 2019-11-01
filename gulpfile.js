c9bb333f8893cb69c9822deac30e711127b6d1e4
Roma <forwm.ru@gmail.com>
2019-11-01 17:27:46 +0300

    setting up gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ const autoprefixer = require('gulp-autoprefixer');
 const gutil = require( 'gulp-util' );
 const svgSprite = require("gulp-svg-sprites");
 const ftp = require( 'vinyl-ftp' );
+const fs = require('fs');
+const env = fs.readFileSync('.env.json', 'utf8') || null;
 
 let settings = {
     localFolder: 'november',
@@ -15,11 +17,11 @@ let settings = {
     })
 };
 
-function deploy() {
+function build() {
     const conn = ftp.create( {
-        host:     'd.mywfc.ru',
-        user:     'root1',
-        password: 'Q8eBVyfb',
+        host:     env.dev.host,
+        user:     env.dev.user,
+        password: env.dev.password,
         parallel: 10,
         log:      gutil.log
     } );
@@ -37,6 +39,7 @@ function deploy() {
 function sprite(){
     return gulp.src(`svg/${settings.localFolder}/*.svg`)
         .pipe(svgSprite({
+            cssFile: "scss/base/sprite.scss",
             padding: 5
         }))
         .pipe(gulp.dest(settings.folder + settings.svg()));
@@ -51,9 +54,16 @@ function scss(){
 }
 
 function watch(){
-    return gulp.watch('scss/**/*.scss', gulp.series(scss, deploy));
+    return gulp.watch('scss/**/*.scss', gulp.series(scss, build));
+}
+
+function deploy(){
+    //TODO create function for server deploy.
 }
 
 
-gulp.task('build', gulp.series(sprite, scss, deploy));
+gulp.task('build', gulp.series(sprite, scss, build));
+
+gulp.task('deploy', deploy);
+
 gulp.task('default', gulp.parallel(watch));
